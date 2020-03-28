51822d08f27ef89e284482ea1bc4ffe95456f881
Anders Børjesson <anbo@zealand.dk>
2020-03-28 09:45:41 +0100

    collectwords with Xamarin forms

--- collectWords3/collectWords3/MainPage.xaml.cs
+++ collectWords3/collectWords3/MainPage.xaml.cs
@@ -9,9 +9,35 @@ namespace collectWords3
 {
     public partial class MainPage : ContentPage
     {
+        private readonly IList<string> _words = new List<string>();
+        //private readonly Result _result = new Result("my name");
+
         public MainPage()
         {
             InitializeComponent();
+            MessageLabel.Text = "my message";
+
+            ButtonAdd.Clicked += (sender, args) =>
+            {
+                _words.Add(WordTextInput.Text);
+            };
+
+            ButtonClear.Clicked += (sender, args) =>
+            {
+                _words.Clear();
+            };
+
+            ButtonShow.Clicked += async (sender, args) =>
+            {
+                MessageLabel.Text = "Words: " + string.Join(",", _words);
+                // https://xamarinhelp.com/passing-data-navigation-xamarin-forms/
+                // MessageLabel.Text = _result.Name;
+
+                ShowWordsPage showPage = new ShowWordsPage(_words/*, _result*/);
+                await Navigation.PushAsync(showPage);
+                //MessageLabel.Text = _result.Name;
+                //MessageLabel.Text = showPage.GetName();       
+            };
         }
     }
 }
---
+++ collectWords3/collectWords3/ShowWordsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace collectWords3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ShowWordsPage : ContentPage
+	{
+        public ShowWordsPage(IList<string> words)
+        {
+            InitializeComponent();
+            string wordsStr = string.Join(", ", words);
+            ShowLabel.Text = wordsStr;
+
+            BackButton.Clicked += async (sender, args) =>
+            {
+                //MessageLabel.Text = "Words: " + string.Join(",", _words);
+                // result.Name = "Anders";
+                // https://xamarinhelp.com/passing-data-navigation-xamarin-forms/
+                await Navigation.PopAsync();
+            };
+        }
+    }
+}
